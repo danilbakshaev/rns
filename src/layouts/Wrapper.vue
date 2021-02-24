ea2f374339c5c1d2873434e76225c4179d355b4c
danilbakshaev <bakshaevdanil@gmail.com>
2021-02-24 20:23:02 +0500

    Добавил шторку в мобилке

--- src/layouts/Wrapper.vue
+++ src/layouts/Wrapper.vue
@@ -13,19 +13,30 @@
       >
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        <router-link to="/favorite" class="btn btn-light mr-2">
-          Закладки
-          <span v-if="this.getFavorite.length" class="badge badge-primary">{{
-            this.getFavorite.length
-          }}</span>
-        </router-link>
-        <router-link to="/basket" class="btn btn-light">
-          Корзина
-          <span v-if="this.getBasket.length" class="badge badge-primary">{{
-            this.getBasket.length
-          }}</span>
-        </router-link>
+      <div
+        class="collapse navbar-collapse justify-content-lg-end"
+        id="navbarNav"
+      >
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link to="/favorite" class="btn btn-light mr-2">
+              Закладки
+              <span
+                v-if="this.getFavorite.length"
+                class="badge badge-primary"
+                >{{ this.getFavorite.length }}</span
+              >
+            </router-link>
+          </li>
+          <li>
+            <router-link to="/basket" class="btn btn-light">
+              Корзина
+              <span v-if="this.getBasket.length" class="badge badge-primary">{{
+                this.getBasket.length
+              }}</span>
+            </router-link>
+          </li>
+        </ul>
       </div>
     </nav>
     <main>
